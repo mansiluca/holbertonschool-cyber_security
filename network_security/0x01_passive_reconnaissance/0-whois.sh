24cc8c014d34be4513813e4dec7d166b0e6358b7
mansiluca <49164932+mansiluca@users.noreply.github.com>
2026-01-20 15:17:57 +0100

    Improve whois script to enhance output formatting and include additional fields

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F ': +' 'BEGIN{OFS=","}/^Registrant |^Admin |^Tech /{print $1,$2}' > "${1}.csv"
+whois "$1" | awk -F': *' '/^(Registrant|Admin|Tech)/ && !/^Registry/ {f=$1; v=$2; if(f~/(Phone Ext|Fax Ext)$/) f=f":"; print f ", " v}' >"$1.csv"
