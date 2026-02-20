9b6d66bcc868a80897ee58fab8e7f529e8f57797
mansiluca <49164932+mansiluca@users.noreply.github.com>
2026-02-20 12:46:10 +0100

    Fix typo in base64 decoding flag in XOR decoder script

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 password="$1"
 password="${password#'{xor}'}"
-echo -n "$password" | base64 -D | od -t u1 -An | while read -r line; do
+echo -n "$password" | base64 -d | od -t u1 -An | while read -r line; do
     for byte in $line; do
         printf "\\$(printf '%03o' "$((byte ^ 95))")"
     done
